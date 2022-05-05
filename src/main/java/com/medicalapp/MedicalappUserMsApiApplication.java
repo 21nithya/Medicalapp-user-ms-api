971a7e0a479656c951ef158b748420456d3f586a
21nithya <nithiyakalyani21@gmail.com>
2022-05-05 18:28:12 +0530

    first commit

---
+++ src/main/java/com/medicalapp/MedicalappUserMsApiApplication.java
@@ -0,0 +1,14 @@
+package com.medicalapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MedicalappUserMsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalappUserMsApiApplication.class, args);
+	}
+
+}
